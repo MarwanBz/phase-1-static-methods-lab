9f9df4bebfb7d47625f5560d94e6a4e2f761d340
MarwanBz <69892218+MarwanBz@users.noreply.github.com>
2023-03-06 19:52:47 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '');
+  }
+
+  static titleize(string) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let result = string.split(' ').map(function(word, index) {
+      if (exceptions.includes(word) && index != 0) {
+        return word;
+      } else {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }
+    }).join(' ');
+    return result;
+  }
 }
